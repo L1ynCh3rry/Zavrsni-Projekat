50f695954140627b399b29d5e765ef5846f9b392
L1ynCh3rry <L1ynCh3rry@DESKTOP-F0EB124>
2018-01-11 21:38:44 +0100

    Finished.

--- js/main.js
+++ js/main.js
@@ -38,34 +38,6 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-
-
-
-
-    $(function () {
-        $('.pop').on('click', function () {
-            $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-            $('#imagemodal').modal('show');
-        });
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
 
 
